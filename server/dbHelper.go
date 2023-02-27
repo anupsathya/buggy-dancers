e39774e3f181970a774afd48bd12ddad329d5554
Anup Sathya <anups@uchicago.edu>
2023-02-27 23:30:01 +0000

    Added a validation check for the ID

--- server/dbHelper.go
+++ server/dbHelper.go
@@ -130,9 +130,12 @@ func dbSetCurrrentBallotByBallot(db *bolt.DB, bal ballotStruct) error {
 
 func dbSetCurrrentBallotByID(db *bolt.DB, id string) error {
 	err := db.Update(func(tx *bolt.Tx) error {
-		err := tx.Bucket([]byte("DB")).Put([]byte("CURRENT_BALLOT"), []byte(id))
-		if err != nil {
-			return fmt.Errorf("could not set current ballot: %v", err)
+		intID, err := strconv.Atoi(id)
+		if intID >= 1000 && intID <= 1017 && err == nil {
+			err := tx.Bucket([]byte("DB")).Put([]byte("CURRENT_BALLOT"), []byte(id))
+			if err != nil {
+				return fmt.Errorf("could not set current ballot: %v", err)
+			}
 		}
 		return nil
 	})
